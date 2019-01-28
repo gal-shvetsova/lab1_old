1ccfbadb605c9256ef6df91d0e98e24089791e80
galya <shvecova.g@gmail.com>
2019-01-28 10:00:05 +0700

    first

--- src/lab1/CSVParser.java
+++ src/lab1/CSVParser.java
@@ -3,19 +3,19 @@ package lab1;
 
 import java.io.*;
 import java.util.*;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class CSVParser {
     private Map<String, Integer> dictionary;
+    private double count = 0;
 
     public void readFile(String filePath) {
         int c;
         int temp = 0;
         String buffer = "";
         try {
-            FileReader input = new FileReader(filePath);;
-            dictionary = new HashMap<String, Integer>();
+            FileReader input = new FileReader(filePath);
+            dictionary = new HashMap<>();
             while ((c = input.read()) != -1) {
                 if (Character.isLetterOrDigit(c))
                     buffer = buffer + (char) c;
@@ -26,6 +26,7 @@ public class CSVParser {
                     dictionary.put(buffer, temp + 1);
                     buffer = "";
                     temp = 0;
+                    count++;
                 }
             }
             input.close();
@@ -37,26 +38,22 @@ public class CSVParser {
     public void writeFile() {
         try {
             FileWriter output = new FileWriter("/home/galya/ru/nsu/ccfit/shvetsova/lab1/src/lab1/out.txt");
-            sort();
-            for (Map.Entry entry: dictionary.entrySet()) {
-                output.write(entry.getKey() + "," + entry.getValue() + '\n');
+            List<Map.Entry> v = sort();
+            double percentage;
+            for (Map.Entry entry : v) {
+                percentage = Double.parseDouble(entry.getValue().toString());
+                percentage = percentage / count * 100.0;
+                output.write(entry.getKey() + "," + entry.getValue() + "," + percentage + '\n');
             }
             output.close();
-        } catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    public void sort(){
-        Integer counter = 0;
-
-        List<Map.Entry> list = dictionary.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toList());
-
-
-        for (Map.Entry v : list){
-            System.out.println(v.getKey() +" - " + v.getValue());
-        }
-
+    public List<Map.Entry> sort() {
+        return dictionary.entrySet().stream().
+                sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toList());
     }
 
 }
